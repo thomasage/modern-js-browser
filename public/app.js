22a417e7b669be38b50700734c25d3ae858495f1
Thomas Agé <thomas.age@protonmail.com>
2023-09-21 20:24:36 -0400

    feat: Add example with local library

--- public/app.js
+++ public/app.js
@@ -1,5 +1,30 @@
 import MixedVinyl from './vinyl.js';
 import _ from 'https://cdn.jsdelivr.net/npm/lodash@4.17.21/+esm';
+import {fileOpen} from 'browser-fs-access';
 
-const mix = new MixedVinyl('Awesome Mix Vol. 1', 2014);
-console.log(_.camelCase(mix.describe()));
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('.input').forEach(element => {
+        element.addEventListener('keyup', onChanged);
+    })
+});
+
+function onChanged() {
+    output(
+        document.getElementById('title').value,
+        document.getElementById('year').value,
+    );
+}
+
+function output(title, year) {
+    const mix = new MixedVinyl(title, year);
+    document.getElementById('output').innerHTML = _.camelCase(mix.describe());
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('browse').addEventListener('click', async () => {
+        const file = await fileOpen({
+            mimeTypes: ['text/plain'],
+        });
+        document.getElementById('content').innerHTML = await file.text();
+    });
+});
